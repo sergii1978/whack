20961f9f7ced92e9afc9ee73add572a32d79dacf
Sergii Storcheus <sens78@gmail.com>
2023-01-28 12:21:15 +0200

    add progress bar

--- whack.rb
+++ whack.rb
@@ -16,16 +16,17 @@ class WhakaMole < Gosu::Window
 		@hammer = Gosu::Image.new("images/hammer.png")
 		@hit = 0
 		@font = Gosu::Font.new(30)
+		@score = 0
 		@playing = true
 		@start_time = 0
 	end
 
 	def draw
 		if @visible > 0
-			@image.draw(@x, @y, 1)
+			@image.draw(@x - @width / 2, @y - @height / 2, 1)
 		end
 
-		@hammer.draw(mouse_x - 25, mouse_y - 25, 1)
+		@hammer.draw(mouse_x - 25, mouse_y - 39, 1)
 
 		if @hit == 0
 			c = Gosu::Color::NONE
@@ -38,14 +39,13 @@ class WhakaMole < Gosu::Window
 		# draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c)
 		Gosu.draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c, z = 0, mode = :default)
 		@hit = 0
-		# @font.draw_text(@score.to_s, 650, 50, 2)
 		@font.draw_text(@score.to_s, 650, 50, 2)
 		@font.draw_text(@time_left.to_s, 50, 50, 2)
 		unless @playing
 			@font.draw_text("Game Over!!!", 300, 300, 3)
 			@visible = 20
 			@time_left = 0
-			@font.draw_text("Press Space Bar To Play Again", 170, 350, 3)
+			@font.draw_text("Press Space Bar To Play Again", 190, 350, 3)
 		end
 	end
 
@@ -57,7 +57,7 @@ class WhakaMole < Gosu::Window
 			@velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
 			@visible -= 1
 			@visible = 30 if @visible < -10 && rand < 0.01
-			@time_left = (10 - ((Gosu.milliseconds - @start_game) / 1000))
+			@time_left = (10 - ((Gosu.milliseconds - @start_time) / 1000))
 			@playing = false if @time_left < 0
 		end
 	end
@@ -67,20 +67,20 @@ class WhakaMole < Gosu::Window
 			if (id == Gosu::MsLeft)
 				if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
 					@hit = 1
-					@score += 10
+					@score = @score + 10
 				else
 					@hit = -1
-					# @score -= 3
+					@score = @score - 3
 				end
 			end
 		else
-			if (id == Gpsu::KbSpace)
+			if (id == Gosu::KbSpace)
 				@playing = true
-				@visible = 10
-				@start_time = Gosu::milliseconds
+				@visible = -10
+				@start_time = Gosu.milliseconds
 				@score = 0
+			end
 		end
-	end
 	end
 
 end
